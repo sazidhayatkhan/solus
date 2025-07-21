f1203ffa213891bdd932bb15164090e4b872f2d8
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-21 12:35:32 +0600

    fixed hero section two in therapists page

--- src/views/page-therapists/SectionOne.tsx
+++ src/views/page-therapists/SectionOne.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 const SectionOne = (props: Props) => {
   return (
-    <div className='mx-4 md:mx-0 mt-4 md:mt-0'>
+    <div className=''>
     <div
-      className="_container relative w-full h-[170px] md:h-[400px] bg-cover bg-top rounded-3xl md:rounded-[40px]"
+      className="_container relative w-full h-[170px] md:h-[400px] bg-cover bg-top rounded-0 md:rounded-[40px]"
       style={{ backgroundImage: "url('/images/therapists.png')" }}
     >
       
