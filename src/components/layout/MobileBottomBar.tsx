f4705b735390a3f73a402906f9a4724bc87cf9e0
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-23 02:31:34 +0600

    color changed

--- src/components/layout/MobileBottomBar.tsx
+++ src/components/layout/MobileBottomBar.tsx
@@ -31,13 +31,13 @@ const MobileBottomBar = (props: Props) => {
   ];
 
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-gray-100 z-[2] block md:hidden border-t border-gray-300">
+    <div className="fixed bottom-0 left-0 w-full bg-primary z-[2] block md:hidden border-t border-gray-300">
       <ul className="flex justify-around items-center">
         {data.map((item, index) => (
           <Link href={item?.linkUrl || ""}>
             <li
               key={index}
-              className="flex flex-col items-center justify-center w-18 h-18 text-gray-500"
+              className="flex flex-col items-center justify-center w-18 h-18 text-white"
             >
               <span className="text-3xl">{item.icon}</span>
             </li>
