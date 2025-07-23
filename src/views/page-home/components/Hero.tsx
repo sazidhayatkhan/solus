f6b746ad363373a9f32e6245cbd68960c3108947
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-07-23 12:09:11 +0600

    fixed width

--- src/views/page-home/components/Hero.tsx
+++ src/views/page-home/components/Hero.tsx
@@ -7,11 +7,11 @@ type Props = {}
 const Hero = (props: Props) => {
   return (
     <div
-      className="_container relative w-full h-[34vh] md:h-[490px] bg-cover bg-center flex items-center justify-center mx-auto rounded-0 md:rounded-[40px]"
+      className="_container relative w-full min-h-[300px] md:h-[490px] bg-cover bg-center flex items-center justify-center mx-auto rounded-0 md:rounded-[40px]"
       style={{ backgroundImage: "url('/images/hero_bg.jpg')" }}
     >
       <div className="text-center px-4">
-        <h1 className="font-brico max-w-[80%] md:max-w-[600px] mx-auto text-3xl md:text-6xl leading-tight font-bold mb-4 text-primary">
+        <h1 className="font-brico max-w-[95%] md:max-w-[600px] mx-auto text-3xl md:text-6xl leading-tight font-bold mb-4 text-primary">
           Support for Your Mental Well-being
         </h1>
         <p className="max-w-[74%] md:max-w-[360px] mx-auto paragraph-base mb-6">
